85515966845d10aedb0cbadabb50746f8cc84623
Lader <laderjk@users.noreply.github.com>
2026-04-21 23:26:47 -0500

    Initial Commit

---
+++ PomoMenu/PomoMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomoMenuApp.swift
+//  PomoMenu
+//
+//  Created by Lader on 21/04/26.
+//
+
+import SwiftUI
+
+@main
+struct PomoMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
